5a926537ee1c8f8c494d20fabc0299d8338be355
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-04-20 17:58:03 +0100

    RESTORE: restored makefile from CPP-MOD-03

---
+++ CPP-MODULE-03/ex00/main.cpp
@@ -0,0 +1,13 @@
+#include "ClapTrap.hpp"
+
+int main() {
+
+	ClapTrap clap("zezinho");
+	ClapTrap newOne("Hugo");
+
+	newOne = clap;
+	newOne.attack("Jheniffer");
+	clap.attack("mario");
+
+	return 0;
+}
