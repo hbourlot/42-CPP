eaf0306852d8bed34f8d93a763ef9d6656d2f50f
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2026-03-25 15:14:21 +0000

    fix: fordjohnson wasnt inserting properly when tks sequence as smaller than 2

--- CPP-MODULE-09/ex02/PmergeMe.cpp
+++ CPP-MODULE-09/ex02/PmergeMe.cpp
@@ -1,6 +1,7 @@
 #include "PmergeMe.hpp"
 
 // ---------- Ford-Johnson sequence ----------
+
 // Book's formula => t_k = (2^(k+1) + (-1)^k) / 3
 template < typename Int > Int fordJohnsonTk( Int k ) {
 	return ( pow( 2, ( k + 1 ) ) + ( ( k % 2 == 0 ) ? 1 : -1 ) ) / 3;
@@ -26,7 +27,7 @@ template < typename IntContainer > IntContainer fordJohnsonSeq( int n ) {
 
 // ---------- Ford-Johnson List ----------
 
-static std::list< Pair > makePairsList( std::list< int > &containerList, bool &hasLeftOver, int &leftover ) {
+static std::list< Pair > makePairsList( std::list< int >& containerList, bool& hasLeftOver, int& leftover ) {
 
 	int first, second;
 	std::list< Pair > pairs;
@@ -58,32 +59,41 @@ static std::list< Pair > makePairsList( std::list< int > &containerList, bool &h
 	return pairs;
 }
 
-static std::list< int > recursiveSortAList( std::list< Pair > &pairs ) {
+static std::list< int > recursiveSortAList( std::list< Pair >& pairs ) {
 	std::list< int > listA;
 	std::list< Pair >::iterator it = pairs.begin();
 	std::list< Pair >::iterator end = pairs.end();
 
 	for ( ; it != end; ++it ) {
-		const Pair &uniquePair = *it;
+		const Pair& uniquePair = *it;
 		listA.push_back( uniquePair.a );
 	}
 	listA = fordJohnsonList( listA );
 	return std::list< int >( listA.begin(), listA.end() );
 }
 
-static std::list< int > insertBsList( std::list<int> mainChain, const std::list< Pair > &pairs ) {
+static std::list< int > insertBsList( std::list< int > mainChain, const std::list< Pair >& pairs ) {
 
 	int n, start, end, bj;
-	std::list<int> tks;
+	std::list< int > tks;
 
 	n = pairs.size();
-	tks = fordJohnsonSeq< std::list<int> >( n );
+	tks = fordJohnsonSeq< std::list< int > >( n );
 
-	if ( tks.size() < 2 )
+	if ( tks.size() < 2 ) {
+		std::list< Pair >::const_iterator it = pairs.begin();
+		if ( it != pairs.end() )
+			++it;
+		for ( ; it != pairs.end(); ++it ) {
+			bj = it->b;
+			std::list< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+			mainChain.insert( pos, bj );
+		}
 		return mainChain;
+	}
 
-	std::list<int>::const_iterator prev = tks.begin();
-	std::list<int>::const_iterator curr = ++tks.begin();
+	std::list< int >::const_iterator prev = tks.begin();
+	std::list< int >::const_iterator curr = ++tks.begin();
 
 	for ( ; curr != tks.end(); ++curr, ++prev ) {
 		start = *prev + 1;
@@ -94,23 +104,25 @@ static std::list< int > insertBsList( std::list<int> mainChain, const std::list<
 
 		for ( int j = end; j >= start; --j, --it ) {
 			bj = it->b;
-			std::list<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+			std::list< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
 			mainChain.insert( pos, bj );
 		}
 	}
 
 	if ( !tks.empty() ) {
-		std::list<int>::iterator last_tks = tks.end();
+		std::list< int >::iterator last_tks = tks.end();
 		--last_tks;
 		if ( *last_tks < n ) {
 			std::list< Pair >::const_iterator it = pairs.end();
 			--it;
 			for ( int j = n; j > *last_tks; --j ) {
-				--it;
 				bj = it->b;
 
-				std::list<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+				std::list< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
 				mainChain.insert( pos, bj );
+				if ( it == pairs.begin() )
+					break;
+				--it;
 			}
 		}
 	}
@@ -139,13 +151,12 @@ std::list< int > fordJohnsonList( std::list< int > containerList ) {
 	mainChain.push_back( pairs.front().b ); // first B
 	mainChain.insert( mainChain.end(), sortedA.begin(), sortedA.end() );
 
-
 	// Step 4: insert b's
-	mainChain = insertBsList(mainChain, pairs);
+	mainChain = insertBsList( mainChain, pairs );
 
 	// Step 5: insert leftover if odd
 	if ( hasLeftOver ) {
-		std::list<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), leftover );
+		std::list< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), leftover );
 
 		mainChain.insert( pos, leftover );
 	}
@@ -155,7 +166,7 @@ std::list< int > fordJohnsonList( std::list< int > containerList ) {
 
 // ---------- Ford-Johnson Deque ----------
 
-static std::deque< Pair > makePairsDeque( std::deque< int > &containerList, bool &hasLeftOver, int &leftover ) {
+static std::deque< Pair > makePairsDeque( std::deque< int >& containerList, bool& hasLeftOver, int& leftover ) {
 
 	int first, second;
 	std::deque< Pair > pairs;
@@ -187,32 +198,41 @@ static std::deque< Pair > makePairsDeque( std::deque< int > &containerList, bool
 	return pairs;
 }
 
-static std::deque< int > recursiveSortADeque( std::deque< Pair > &pairs ) {
+static std::deque< int > recursiveSortADeque( std::deque< Pair >& pairs ) {
 	std::deque< int > listA;
 	std::deque< Pair >::iterator it = pairs.begin();
 	std::deque< Pair >::iterator end = pairs.end();
 
 	for ( ; it != end; ++it ) {
-		const Pair &uniquePair = *it;
+		const Pair& uniquePair = *it;
 		listA.push_back( uniquePair.a );
 	}
 	listA = fordJohnsonDeque( listA );
 	return std::deque< int >( listA.begin(), listA.end() );
 }
 
-static std::deque< int > insertBsDeque( std::deque<int> mainChain, const std::deque< Pair > &pairs ) {
+static std::deque< int > insertBsDeque( std::deque< int > mainChain, const std::deque< Pair >& pairs ) {
 
 	int n, start, end, bj;
-	std::deque<int> tks;
+	std::deque< int > tks;
 
 	n = pairs.size();
-	tks = fordJohnsonSeq< std::deque<int> >( n );
+	tks = fordJohnsonSeq< std::deque< int > >( n );
 
-	if ( tks.size() < 2 )
+	if ( tks.size() < 2 ) {
+		std::deque< Pair >::const_iterator it = pairs.begin();
+		if ( it != pairs.end() )
+			++it;
+		for ( ; it != pairs.end(); ++it ) {
+			bj = it->b;
+			std::deque< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+			mainChain.insert( pos, bj );
+		}
 		return mainChain;
+	}
 
-	std::deque<int>::const_iterator prev = tks.begin();
-	std::deque<int>::const_iterator curr = ++tks.begin();
+	std::deque< int >::const_iterator prev = tks.begin();
+	std::deque< int >::const_iterator curr = ++tks.begin();
 
 	for ( ; curr != tks.end(); ++curr, ++prev ) {
 		start = *prev + 1;
@@ -223,23 +243,24 @@ static std::deque< int > insertBsDeque( std::deque<int> mainChain, const std::de
 
 		for ( int j = end; j >= start; --j, --it ) {
 			bj = it->b;
-			std::deque<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+			std::deque< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
 			mainChain.insert( pos, bj );
 		}
 	}
 
 	if ( !tks.empty() ) {
-		std::deque<int>::iterator last_tks = tks.end();
+		std::deque< int >::iterator last_tks = tks.end();
 		--last_tks;
 		if ( *last_tks < n ) {
 			std::deque< Pair >::const_iterator it = pairs.end();
 			--it;
 			for ( int j = n; j > *last_tks; --j ) {
-				--it;
 				bj = it->b;
-
-				std::deque<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
+				std::deque< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), bj );
 				mainChain.insert( pos, bj );
+				if ( it == pairs.begin() )
+					break;
+				--it;
 			}
 		}
 	}
@@ -268,13 +289,12 @@ std::deque< int > fordJohnsonDeque( std::deque< int > containerList ) {
 	mainChain.push_back( pairs.front().b ); // first B
 	mainChain.insert( mainChain.end(), sortedA.begin(), sortedA.end() );
 
-
 	// Step 4: insert b's
-	mainChain = insertBsDeque(mainChain, pairs);
+	mainChain = insertBsDeque( mainChain, pairs );
 
 	// Step 5: insert leftover if odd
 	if ( hasLeftOver ) {
-		std::deque<int>::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), leftover );
+		std::deque< int >::iterator pos = std::lower_bound( mainChain.begin(), mainChain.end(), leftover );
 
 		mainChain.insert( pos, leftover );
 	}
