23a927018466b6d2f7ad1fc67944258e6f9a3f26
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2026-04-14 15:30:11 +0100

    Remove: removed declared function in header file
    removed error clas

--- CPP-MODULE-09/ex00/BitcoinExchange.cpp
+++ CPP-MODULE-09/ex00/BitcoinExchange.cpp
@@ -2,6 +2,181 @@
 #include <fstream>
 #include <stdexcept>
 
+static long ft_stol( const std::string &src ) {
+	long result;
+	bool isNegative;
+	size_t i;
+
+	result = 0;
+	isNegative = false;
+	i = 0;
+
+	if ( src.empty() )
+		return 0;
+
+	if ( src[0] == '-' ) {
+		isNegative = true;
+		i++;
+	}
+
+	for ( ; i < src.size(); i++ ) {
+		if ( src[i] < '0' || src[i] > '9' )
+			break; // stop at first non-digit
+		result = result * 10 + ( src[i] - '0' );
+	}
+
+	return isNegative ? -result : result;
+}
+
+static bool isBi(int year) {
+	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+}
+
+static int getDaywithMonth(int year, int month) {
+	
+	int monthsDays[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	if (isBi(year) && month == 1) {
+		return 29;
+	};
+
+	return monthsDays[month];
+}
+
+static std::string getDate( std::string line, const char sep ) {
+
+	size_t sepPos;
+	std::string date = "";
+
+	sepPos = line.find( sep );
+
+	if ( sepPos == std::string::npos )
+		return date;
+
+	return line.substr( 0, sepPos );
+};
+
+static std::string trim( const std::string &s ) {
+
+	size_t start, end;
+
+	start = s.find_first_not_of( " \t\n\r\v" );
+	if ( start == std::string::npos )
+		return "";
+
+	end = s.find_last_not_of( " \t\n\r\f\v" );
+
+	return s.substr( start, end - start + 1 );
+}
+
+static void displayOutput( std::string inputLine, std::string &date, float exchangeValue ) {
+
+	std::string display;
+	size_t sepPos = inputLine.find( '|' );
+
+	display = date + " => ";
+	display += trim( inputLine.substr( sepPos + 1 ) );
+	display += " = ";
+	std::cout << display << exchangeValue << std::endl;
+};
+
+static float getValue( const std::string &line, char sep ) {
+
+	std::string valueStr;
+	size_t sepPos;
+
+	sepPos = line.find( sep );
+	if ( sepPos == std::string::npos ) {
+		std::cerr << "Error: missing separator in line: " << line << std::endl;
+		return VALUE_ERROR;
+	}
+	try {
+
+		valueStr = trim( line.substr( sepPos + 1 ) );
+		std::stringstream ss( valueStr );
+		float value;
+		ss >> value;
+		if ( ss.fail() || !ss.eof() ) {
+			throw std::exception();
+		}
+		return value;
+	} catch ( const std::exception &e ) {
+		std::cerr << "Error: invalid number in line: " << line << std::endl;
+		return VALUE_ERROR;
+	}
+};
+
+static bool checkDateFormat( const std::string &date ) {
+
+	std::string format;
+	struct tm tm = {};
+
+
+	format = "%Y-%m-%d";
+
+	std::string localDate = trim(date);
+	
+	if (localDate.length() != 10 || localDate[4] != '-' || localDate[7] != '-')
+        return false;
+
+	if ( !strptime( date.c_str(), format.c_str(), &tm ) )
+		return ERROR;
+	
+	int day;
+	std::string sday = localDate.substr(7 + 1);
+	day = atoi(sday.c_str());
+
+	if (day > getDaywithMonth(tm.tm_year + 1900, tm.tm_mon))
+		return ERROR;
+
+	return SUCCESS;
+};
+
+static size_t getSumTotalTime( std::string time ) {
+
+	size_t firstDash, lastDash, totalTime;
+	std::string year, month, day;
+
+	firstDash = time.find( '-' );
+	lastDash = time.rfind( '-' );
+
+	year = time.substr( 0, firstDash );
+	month = time.substr( firstDash + 1, lastDash - firstDash - 1 );
+	day = time.substr( lastDash + 1 );
+
+	std::string concatenatedString = year + month + day;
+
+	totalTime = ft_stol( concatenatedString );
+	if ( totalTime == std::string::npos )
+		return 1.0f;
+	return totalTime;
+};
+
+static float getClosestRate( const std::string &date, std::map< std::string, float > &btcContainer ) {
+
+	size_t inputTotalDate = getSumTotalTime( date );
+	std::string closestDate;
+	float closestValue = 0.0f;
+
+	for ( std::map< std::string, float >::iterator it = btcContainer.begin(); it != btcContainer.end(); ++it ) {
+		size_t btcTotalDate = getSumTotalTime( it->first );
+
+		if ( btcTotalDate <= inputTotalDate ) {
+			closestDate = it->first;
+			closestValue = it->second;
+		} else {
+			break;
+		}
+	}
+
+	if ( closestDate.empty() ) {
+		return VALUE_ERROR;
+	}
+
+	return closestValue;
+};
+
+
 BitcoinExchange::BitcoinExchange( char *inputFilePath )
 	: _inputFilePath( inputFilePath ) {
 
@@ -43,87 +218,14 @@ bool BitcoinExchange::loadBtcDatabase( std::ifstream &file ) {
 	return SUCCESS;
 };
 
-float getValue( const std::string &line, char sep ) {
-
-	std::string valueStr;
-	size_t sepPos;
-
-	sepPos = line.find( sep );
-	if ( sepPos == std::string::npos ) {
-		std::cerr << "Error: missing separator in line: " << line << std::endl;
-		return VALUE_ERROR;
-	}
-	try {
-
-		valueStr = trim( line.substr( sepPos + 1 ) );
-		std::stringstream ss( valueStr );
-		float value;
-		ss >> value;
-		if ( ss.fail() || !ss.eof() ) {
-			throw std::exception();
-		}
-		return value;
-	} catch ( const std::exception &e ) {
-		std::cerr << "Error: invalid number in line: " << line << std::endl;
-		return VALUE_ERROR;
-	}
-};
 
 
-static bool isBi(int year) {
-	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-}
-
-static int getDaywithMonth(int year, int month) {
-	
-	int monthsDays[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	
-	if (isBi(year) && month == 1) {
-		return 29;
-	};
-
-	return monthsDays[month];
-}
 
 
-bool checkDateFormat( const std::string &date ) {
-
-	std::string format;
-	struct tm tm = {};
 
 
-	format = "%Y-%m-%d";
 
-	std::string localDate = trim(date);
-	
-	if (localDate.length() != 10 || localDate[4] != '-' || localDate[7] != '-')
-        return false;
 
-	if ( !strptime( date.c_str(), format.c_str(), &tm ) )
-		return ERROR;
-	
-	int day;
-	std::string sday = localDate.substr(7 + 1);
-	day = atoi(sday.c_str());
-
-	if (day > getDaywithMonth(tm.tm_year + 1900, tm.tm_mon))
-		return ERROR;
-
-	return SUCCESS;
-};
-
-std::string getDate( std::string line, const char sep ) {
-
-	size_t sepPos;
-	std::string date = "";
-
-	sepPos = line.find( sep );
-
-	if ( sepPos == std::string::npos )
-		return date;
-
-	return line.substr( 0, sepPos );
-};
 
 bool openFile( std::ifstream &file, const std::string &path ) {
 
@@ -134,16 +236,7 @@ bool openFile( std::ifstream &file, const std::string &path ) {
 	return true;
 };
 
-void displayOutput( std::string inputLine, std::string &date, float exchangeValue ) {
 
-	std::string display;
-	size_t sepPos = inputLine.find( '|' );
-
-	display = date + " => ";
-	display += trim( inputLine.substr( sepPos + 1 ) );
-	display += " = ";
-	std::cout << display << exchangeValue << std::endl;
-};
 
 static bool parseBadInput( const std::string &line ) {
 
@@ -237,61 +330,26 @@ void BitcoinExchange::processTransactions( std::ifstream &inputFile ) {
 	};
 };
 
-std::string trim( const std::string &s ) {
 
-	size_t start, end;
 
-	start = s.find_first_not_of( " \t\n\r\v" );
-	if ( start == std::string::npos )
-		return "";
 
-	end = s.find_last_not_of( " \t\n\r\f\v" );
-
-	return s.substr( start, end - start + 1 );
-}
-
-long ft_stol( const std::string &src ) {
-	long result;
-	bool isNegative;
-	size_t i;
-
-	result = 0;
-	isNegative = false;
-	i = 0;
-
-	if ( src.empty() )
-		return 0;
-
-	if ( src[0] == '-' ) {
-		isNegative = true;
-		i++;
-	}
-
-	for ( ; i < src.size(); i++ ) {
-		if ( src[i] < '0' || src[i] > '9' )
-			break; // stop at first non-digit
-		result = result * 10 + ( src[i] - '0' );
-	}
-
-	return isNegative ? -result : result;
-}
 
 void BitcoinExchange::run() {
 
 	std::ifstream btcFile, inputFile;
 
 	if ( !openFile( btcFile, DATA_PATH ) )
-		throw BtcError::OpenFile( DATA_PATH );
+		throw std::runtime_error("Error: could not open file.");
 
 	if ( !openFile( inputFile, _inputFilePath ) ) {
 		btcFile.close();
-		throw BtcError::OpenFile( _inputFilePath );
+		throw std::runtime_error("Error: could not open file.");
 	};
 
 	if ( !loadBtcDatabase( btcFile ) ) {
 		btcFile.close();
 		inputFile.close();
-		throw BtcError::Base( "Error: Wrong btcDataBate." );
+		throw std::runtime_error("Error: Wrong btcDataBate.");
 	};
 
 	processTransactions( inputFile );
@@ -311,46 +369,3 @@ bool parseDateFormat( std::string &date ) {
 	return true;
 };
 
-static size_t getSumTotalTime( std::string time ) {
-
-	size_t firstDash, lastDash, totalTime;
-	std::string year, month, day;
-
-	firstDash = time.find( '-' );
-	lastDash = time.rfind( '-' );
-
-	year = time.substr( 0, firstDash );
-	month = time.substr( firstDash + 1, lastDash - firstDash - 1 );
-	day = time.substr( lastDash + 1 );
-
-	std::string concatenatedString = year + month + day;
-
-	totalTime = ft_stol( concatenatedString );
-	if ( totalTime == std::string::npos )
-		return 1.0f;
-	return totalTime;
-};
-
-float getClosestRate( const std::string &date, std::map< std::string, float > &btcContainer ) {
-
-	size_t inputTotalDate = getSumTotalTime( date );
-	std::string closestDate;
-	float closestValue = 0.0f;
-
-	for ( std::map< std::string, float >::iterator it = btcContainer.begin(); it != btcContainer.end(); ++it ) {
-		size_t btcTotalDate = getSumTotalTime( it->first );
-
-		if ( btcTotalDate <= inputTotalDate ) {
-			closestDate = it->first;
-			closestValue = it->second;
-		} else {
-			break;
-		}
-	}
-
-	if ( closestDate.empty() ) {
-		return VALUE_ERROR;
-	}
-
-	return closestValue;
-};
--- CPP-MODULE-09/ex00/BitcoinExchange.hpp
+++ CPP-MODULE-09/ex00/BitcoinExchange.hpp
@@ -59,49 +59,7 @@ class BitcoinExchange {
 		void processTransactions( std::ifstream &inputFile );
 };
 
-namespace BtcError {
-
-	class Base : public std::runtime_error {
-	  public:
-		explicit Base(const std::string &msg) : std::runtime_error(msg) {
-		}
-		explicit Base(const std::string &msg, const std::string &input) : std::runtime_error(msg + " '" + input + "'\n") {
-		}
-	};
-
-	/// @brief Exception thrown when a file cannot be opened.
-	/// This exception is used to indicate file opening failures (e.g., missing or inaccessible files).
-	class OpenFile : public Base {
-	  public:
-		explicit OpenFile(const std::string &file = "") : Base("Cout not open file", file) {
-		};
-	};
-
-	/// @brief Exception used when there is missing information in the input line.
-	/// This exception should be thrown if the input line does not contain all required fields.
-	class BadInput : public Base {
-	  public:
-		explicit BadInput(const std::string &input = "") : Base("Bad input => " + input) {
-		}
-	};
-
-} // namespace BtcError
 
 
-bool openFile( std::ifstream &file, const std::string &path );
 
-bool parseDateFormat( std::string &date );
 
-std::string trim( const std::string &s );
-
-std::string getDate( std::string line, const char sep );
-
-float getValue( const std::string &line, char sep );
-
-bool checkDateFormat( const std::string &date );
-
-float getClosestRate( const std::string &date, std::map< std::string, float > &btcContainer );
-
-void displayOutput( std::string inputLine, std::string &date, float exchangeValue );
-
-long ft_stol( const std::string &src );
